bd9adcfc715d23a72e7abed978b1f49330554ec4
Milan Topuzov <topuzov.work@gmail.com>
2015-06-18 12:50:43 +0200

    Final refactoring. App working 100%

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -21,22 +21,23 @@ class Scraper(object):
         log.debug('Got json data from %s.', url)
         return data
 
-    def generate_keyword(self, keyword, term, position):
+    def generate_keyword(self, keyword, result):
         '''Return 'term keyword' if position is 0 else 'keyword term'.'''
-        if position == 0:
-            return '{} {}'.format(term, keyword)
-        return '{} {}'.format(keyword, term)
+        if result['position'] == 0:
+            return '{} {}'.format(result['term'], keyword)
+        return '{} {}'.format(keyword, result['term'])
 
     def save_keywords(self, keyword, json):
         '''Get and save generated keyword objects.'''
-        for result in json['resourceDataCache'][0]['data']['guides']:
+        data = json['resourceDataCache'][0]['data']['guides'] or []
+        for result in data:
+            generated_keyword = self.generate_keyword(keyword, result)
             Keyword.objects.create(
-                keyword=self.generate_keyword(
-                    keyword, result['term'], result['position']
-                ),
+                keyword=generated_keyword,
                 category=keyword.category,
                 scraped=True
             )
+            log.debug('Saved keyword %s', generated_keyword)
 
     def __call__(self, keyword):
         '''Run code for Scraper.'''
